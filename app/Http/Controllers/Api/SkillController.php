b8e9413edda7f4c995cf4d24d4f57f7bfe017acc
EarlGimenez <earl.reynan123@gmail.com>
2026-01-23 00:18:33 +0800

    refactor: update SkillController to use Skill model and remove unused credentials migration

--- app/Http/Controllers/Api/SkillController.php
+++ app/Http/Controllers/Api/SkillController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\UserSkill;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
