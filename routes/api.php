ec027a5672df6b980ccf9dd3f359626ed32c374f
YANA <alliyanagarcia@gmail.com>
2026-01-22 17:04:22 +0800

    job matching algorithm implemented

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\JobController;
 use App\Http\Controllers\Api\SeminarController;
 use App\Http\Controllers\Api\SessionController;
+use App\Http\Controllers\Api\AgentMatchController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,6 @@ Route::get('/seminars', [SeminarController::class, 'index']);
 Route::get('/seminars/{id}', [SeminarController::class, 'show']);
 Route::post('/seminars/{id}/register', [SeminarController::class, 'register']);
 Route::post('/seminars/{id}/verify', [SeminarController::class, 'verify']);
+
+// Agent triggers
+Route::post('/agent/match-score', [AgentMatchController::class, 'score']);
